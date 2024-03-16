f24cac06213950aa820e33433937642747932174
Ryan Ehrlich <ryansehrlich@gmail.com>
2024-03-15 23:56:30 -0400

    Handles and links

--- scripts/narratives.js
+++ scripts/narratives.js
@@ -1,18 +1,44 @@
-function makeButtonElement() {
-    let buttonDiv = document.createElement('div');
-    buttonDiv.className = "diffbar-item dropdown js-reviews-container";
-    buttonDiv.innerHTML = `<button id="narratives-button" style="border-top-right-radius: 0; border-bottom-right-radius: 0" target="_blank" aria-label="Copy Narratives Order" data-view-component="true" class="Button--secondary Button--small Button">
+function makeButtonHTML(content) {
+    return `<button target="_blank" aria-label="Copy Narratives Order" data-view-component="true" class="Button--secondary Button--small Button">
     <span class="Button-content">
        <span class="Button-label">
+          ${content}
+       </span>
+    </span>
+ </button>`;
+}
+function makeControlsDiv() {
+    const handleControlDiv = document.createElement('div');
+    handleControlDiv.className = "diffbar-item dropdown js-reviews-container";
+    handleControlDiv.innerHTML = makeButtonHTML("Reorder diffs") + makeButtonHTML("Hide handles");
+    handleControlDiv.setAttribute("style", "margin-right: 8px; margin-left: 8px");
+    const showButton = handleControlDiv.children[0];
+    const hideButton = handleControlDiv.children[1];
+    hideButton.style.display = "none";
+    showButton.addEventListener("click", () => {
+        document.querySelectorAll(".narratives-dnd-handle").forEach(handle => handle.style.display = "inline");
+        showButton.style.display = "none";
+        hideButton.style.display = "inline-block";
+    });
+    hideButton.addEventListener("click", () => {
+        document.querySelectorAll(".narratives-dnd-handle").forEach(handle => handle.style.display = "none");
+        showButton.style.display = "inline-block";
+        hideButton.style.display = "none";
+    });
+    return { handleControlDiv };
+}
+function makeCopyButton() {
+    let copyButtonDiv = document.createElement('div');
+    copyButtonDiv.className = "diffbar-item dropdown js-reviews-container";
+    copyButtonDiv.innerHTML = makeButtonHTML(`
           <svg aria-hidden="true" focusable="false" role="img" class="octicon octicon-copy" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display:inline-block;user-select:none;vertical-align:text-bottom;overflow:visible">
              <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path>
              <path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
           </svg>
-       </span>
-    </span>
- </button>`;
-    let button = buttonDiv.querySelector('button');
-    return { buttonDiv, button };
+       `);
+    let copyButton = copyButtonDiv.querySelector('button');
+    copyButton.setAttribute("style", "border-top-right-radius: 0; border-bottom-right-radius: 0");
+    return { copyButtonDiv, copyButton };
 }
 function makeInputElement() {
     let inputDiv = document.createElement("div");
@@ -29,6 +55,20 @@ function makeInputElement() {
         >`;
     let input = inputDiv.querySelector("input");
     return { inputDiv, input };
+}
+function makeDragAndDropHandle() {
+    const span = document.createElement("span");
+    span.innerHTML = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-three-bars Button-visual">
+        <path d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z"></path>
+    </svg>`;
+    span.className = "narratives-dnd-handle";
+    span.setAttribute("style", "cursor: grab; color: var(--fgColor-muted, var(--color-fg-muted)); display: none");
+    return span;
+}
+function setupHandles() {
+    document.querySelectorAll(".file-header").forEach(container => {
+        container.insertBefore(makeDragAndDropHandle(), container.firstChild);
+    });
 }
 const MAX_ITERS = 100; // prevent infinite loops
 /**
@@ -50,23 +90,16 @@ function combineContainers() {
         }
     });
 }
-const events_to_stop_propagation_in_header = ["drag", "dragstart", "mousedown", "touchstart", "pointerdown", "click"];
+const events_to_stop_propagation_in_header = ["drag", "dragstart", "mousedown", "touchstart", "pointerdown"];
 /**
  * When someone starts dragging, we do two things:
  * - collapse all diffs, so just the header shows
  * - remove the content of the files from the DOM to make the dragging faster
  *
- * We then lazily add back the content when someone clicks the expand button
  */
 function setupDiffContentRemover() {
     const containersAndElements = [];
     document.querySelectorAll("div.file.js-file").forEach(container => {
-        // children: [header, content]
-        const header = container.children[0];
-        // Don't cause any input element in the header to start a drag
-        header.querySelectorAll("button, clipboard-copy, a, input, form, .js-file-header-dropdown, tool-tip").forEach(elem => {
-            events_to_stop_propagation_in_header.forEach(key => elem.addEventListener(key, (event) => event.stopPropagation()));
-        });
         containersAndElements.push({
             container,
             element: container.children[1]
@@ -81,10 +114,11 @@ function setupDiffContentRemover() {
     };
     const onDragEnd = () => {
         // add all the content back we deleted
-        containersAndElements.forEach(({ container, element }) => container.appendChild(element));
+        containersAndElements.forEach(({ container, element }) => setTimeout(() => container.appendChild(element), 0));
     };
     return { onDragStart, onDragEnd };
 }
+var narrativeFromQueryString = null;
 function setupNarratives() {
     let inputParent = document.querySelector(".pr-review-tools");
     if (inputParent === null) {
@@ -96,17 +130,20 @@ function setupNarratives() {
     }
     inputParent.setAttribute("narratives-setup", "true");
     combineContainers();
+    setupHandles();
     let { inputDiv, input } = makeInputElement();
-    let { buttonDiv, button } = makeButtonElement();
+    let { copyButtonDiv, copyButton } = makeCopyButton();
+    const { handleControlDiv } = makeControlsDiv();
     inputParent.insertBefore(inputDiv, inputParent.children[1]);
-    inputParent.insertBefore(buttonDiv, inputParent.children[1]);
+    inputParent.insertBefore(copyButtonDiv, inputParent.children[1]);
+    inputParent.insertBefore(handleControlDiv, inputParent.children[1]);
     const { onDragStart, onDragEnd } = setupDiffContentRemover();
     // @ts-ignore: sortable is imported by chrome
     let sortable = Sortable.create(document.getElementsByClassName("js-diff-progressive-container")[0], {
         dataIdAttr: 'data-file-path',
-        handle: ".file-header.file-header--expandable",
+        handle: ".narratives-dnd-handle",
         onUpdate() {
-            input.value = JSON.stringify(sortable.toArray());
+            input.value = encodeURIComponent(JSON.stringify(sortable.toArray()));
         },
         onChoose: function () {
             onDragStart();
@@ -115,14 +152,18 @@ function setupNarratives() {
             onDragEnd();
         }
     });
-    button.addEventListener("click", () => {
+    copyButton.addEventListener("click", () => {
         input.select();
         input.setSelectionRange(0, 1000000); // per w3 school
         navigator.clipboard.writeText(input.value);
     });
     function setOrder(orderAsString) {
-        let result = JSON.parse(orderAsString);
+        let result = JSON.parse(decodeURIComponent(orderAsString));
         sortable.sort(result);
+    }
+    if (narrativeFromQueryString !== null) {
+        setOrder(narrativeFromQueryString);
+        input.value = narrativeFromQueryString;
     }
     input.addEventListener("paste", (event) => {
         if (event.clipboardData !== null) {
@@ -159,8 +200,16 @@ function waitForProgressiveContainerThenSetupNarratives() {
     }
     check();
 }
+function extractNarrativeFromQueryString() {
+    const searchParams = new URLSearchParams(document.location.search);
+    const diffOrder = searchParams.get("diff-order");
+    if (diffOrder !== null) {
+        narrativeFromQueryString = diffOrder;
+    }
+}
 // @ts-ignore: window.navigation exists on up to date versions of chrome
 window.navigation.addEventListener("navigate", function () {
+    extractNarrativeFromQueryString();
     // only run on pull request page
     if (document.location.pathname.match(/^\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/pull\/\d+\/files$/)) {
         setTimeout(waitForProgressiveContainerThenSetupNarratives, 0);
--- scripts/narratives.ts
+++ scripts/narratives.ts
@@ -1,19 +1,58 @@
-function makeButtonElement() {
-    let buttonDiv = document.createElement('div');
-    buttonDiv.className = "diffbar-item dropdown js-reviews-container";
-    buttonDiv.innerHTML = `<button id="narratives-button" style="border-top-right-radius: 0; border-bottom-right-radius: 0" target="_blank" aria-label="Copy Narratives Order" data-view-component="true" class="Button--secondary Button--small Button">
+function makeButtonHTML(content) {
+    return `<button target="_blank" aria-label="Copy Narratives Order" data-view-component="true" class="Button--secondary Button--small Button">
     <span class="Button-content">
        <span class="Button-label">
+          ${content}
+       </span>
+    </span>
+ </button>`
+}
+
+function makeControlsDiv() {
+    const handleControlDiv = document.createElement('div');
+    handleControlDiv.className = "diffbar-item dropdown js-reviews-container";
+    handleControlDiv.innerHTML = makeButtonHTML("Reorder diffs") + makeButtonHTML("Hide handles")
+    handleControlDiv.setAttribute("style", "margin-right: 8px; margin-left: 8px")
+
+    const showButton = handleControlDiv.children[0] as  HTMLButtonElement
+    const hideButton = handleControlDiv.children[1] as  HTMLButtonElement
+
+    hideButton.style.display = "none"
+
+    showButton.addEventListener("click", () => {
+        document.querySelectorAll(".narratives-dnd-handle").forEach(handle => 
+            (handle as HTMLSpanElement).style.display = "inline"
+        )
+
+        showButton.style.display = "none"
+        hideButton.style.display = "inline-block"
+    })
+
+    hideButton.addEventListener("click", () => {
+        document.querySelectorAll(".narratives-dnd-handle").forEach(handle => 
+            (handle as HTMLSpanElement).style.display = "none"
+        )
+
+        showButton.style.display = "inline-block"
+        hideButton.style.display = "none"
+    })
+
+    return { handleControlDiv };
+}
+
+function makeCopyButton() {
+    let copyButtonDiv = document.createElement('div');
+    copyButtonDiv.className = "diffbar-item dropdown js-reviews-container";
+    copyButtonDiv.innerHTML = makeButtonHTML(`
           <svg aria-hidden="true" focusable="false" role="img" class="octicon octicon-copy" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display:inline-block;user-select:none;vertical-align:text-bottom;overflow:visible">
              <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path>
              <path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
           </svg>
-       </span>
-    </span>
- </button>`;
-    let button = buttonDiv.querySelector('button') as HTMLButtonElement;
+       `);
+    let copyButton = copyButtonDiv.querySelector('button') as HTMLButtonElement;
+    copyButton.setAttribute("style", "border-top-right-radius: 0; border-bottom-right-radius: 0")
 
-    return { buttonDiv, button };
+    return { copyButtonDiv, copyButton };
 }
 
 function makeInputElement() {
@@ -32,6 +71,23 @@ function makeInputElement() {
     let input = inputDiv.querySelector("input") as HTMLInputElement;
     return { inputDiv, input };
 }
+
+function makeDragAndDropHandle() {
+    const span = document.createElement("span")
+    span.innerHTML = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-three-bars Button-visual">
+        <path d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z"></path>
+    </svg>`
+    span.className = "narratives-dnd-handle"
+    span.setAttribute("style", "cursor: grab; color: var(--fgColor-muted, var(--color-fg-muted)); display: none")
+    return span
+}
+
+function setupHandles() {
+    document.querySelectorAll(".file-header").forEach(container => {
+        container.insertBefore(makeDragAndDropHandle(), container.firstChild)
+    })
+}
+
 
 const MAX_ITERS = 100 // prevent infinite loops
 
@@ -56,31 +112,19 @@ function combineContainers() {
     })
 }
 
-const events_to_stop_propagation_in_header = ["drag", "dragstart", "mousedown", "touchstart", "pointerdown", "click"]
+
+const events_to_stop_propagation_in_header = ["drag", "dragstart", "mousedown", "touchstart", "pointerdown"]
 
 /**
  * When someone starts dragging, we do two things:
  * - collapse all diffs, so just the header shows
  * - remove the content of the files from the DOM to make the dragging faster
  * 
- * We then lazily add back the content when someone clicks the expand button
  */
 function setupDiffContentRemover() {
     const containersAndElements: { container: Element, element: Element }[] = []
 
     document.querySelectorAll("div.file.js-file").forEach(container => {
-        // children: [header, content]
-        const header = container.children[0]
-
-        // Don't cause any input element in the header to start a drag
-        header.querySelectorAll(
-            "button, clipboard-copy, a, input, form, .js-file-header-dropdown, tool-tip"
-        ).forEach(elem => {
-            events_to_stop_propagation_in_header.forEach(
-                key => elem.addEventListener(key, (event) => event.stopPropagation())
-            )
-        })
-
         containersAndElements.push({
             container,
             element: container.children[1]
@@ -101,13 +145,14 @@ function setupDiffContentRemover() {
     const onDragEnd = () => {
         // add all the content back we deleted
         containersAndElements.forEach(
-            ({ container, element }) => container.appendChild(element)
+            ({ container, element }) => setTimeout(() => container.appendChild(element), 0)
         )
     }
 
     return { onDragStart, onDragEnd }
 }
 
+var narrativeFromQueryString: null | string = null
 
 function setupNarratives() {
     let inputParent = document.querySelector(".pr-review-tools")
@@ -123,12 +168,16 @@ function setupNarratives() {
     inputParent.setAttribute("narratives-setup", "true")
 
     combineContainers()
+    setupHandles()
 
     let { inputDiv, input } = makeInputElement()
-    let { buttonDiv, button } = makeButtonElement()
+    let { copyButtonDiv, copyButton } = makeCopyButton()
+
+    const { handleControlDiv } = makeControlsDiv()
 
     inputParent.insertBefore(inputDiv, inputParent.children[1])
-    inputParent.insertBefore(buttonDiv, inputParent.children[1])
+    inputParent.insertBefore(copyButtonDiv, inputParent.children[1])
+    inputParent.insertBefore(handleControlDiv, inputParent.children[1])
 
     const { onDragStart, onDragEnd } = setupDiffContentRemover()
 
@@ -137,9 +186,9 @@ function setupNarratives() {
         document.getElementsByClassName("js-diff-progressive-container")[0],
         {
             dataIdAttr: 'data-file-path',
-            handle: ".file-header.file-header--expandable",
+            handle: ".narratives-dnd-handle",
             onUpdate() {
-                input.value = JSON.stringify(sortable.toArray())
+                input.value = encodeURIComponent(JSON.stringify(sortable.toArray()))
             },
             onChoose: function () {
                 onDragStart()
@@ -151,16 +200,20 @@ function setupNarratives() {
     )
 
 
-    button.addEventListener("click", () => {
+    copyButton.addEventListener("click", () => {
         input.select();
         input.setSelectionRange(0, 1000000); // per w3 school
         navigator.clipboard.writeText(input.value);
     })
 
     function setOrder(orderAsString) {
-        let result = JSON.parse(orderAsString)
+        let result = JSON.parse(decodeURIComponent(orderAsString))
         sortable.sort(result)
+    }
 
+    if (narrativeFromQueryString !== null) {
+        setOrder(narrativeFromQueryString)
+        input.value = narrativeFromQueryString
     }
 
     input.addEventListener("paste", (event) => {
@@ -204,8 +257,18 @@ function waitForProgressiveContainerThenSetupNarratives() {
     check()
 }
 
+function extractNarrativeFromQueryString() {
+    const searchParams = new URLSearchParams(document.location.search)
+    const diffOrder = searchParams.get("diff-order")
+    if (diffOrder !== null) {
+        narrativeFromQueryString = diffOrder
+    }
+}
+
 // @ts-ignore: window.navigation exists on up to date versions of chrome
 window.navigation.addEventListener("navigate", function () {
+    extractNarrativeFromQueryString()
+
     // only run on pull request page
     if (document.location.pathname.match(/^\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/pull\/\d+\/files$/)) {
         setTimeout(waitForProgressiveContainerThenSetupNarratives, 0)
